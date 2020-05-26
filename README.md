18ffb28a768b40121a928426ed8d08a46e52a712
Michael Eby <michaeleby1@gmail.com>
2020-05-26 14:28:04 -0400

    moved functions to .py file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# django-practice
+# Write XML
---
+++ utils.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+#############################
+#  HELPER FUNCTIONS         #
+#############################
+
+
+def to_xml(row):
+    """
+    Converts row of Pandas DataFrame to XML format
+    """
+    xml = ['<item>']
+    for col in row.index:
+        xml.append(f'  <field name="{col}">{row[col]}</field>')
+    xml.append('</item>')
+    return '\n'.join(xml)
+
+
+def df_to_xml(df):
+    """
+    Converts entire Pandas DataFrame to XML format
+    """
+    return '\n'.join(df.apply(to_xml, axis=1))
+
+
+def write_xml(doc, filename, mode='w'):
+    """
+    Writes XML file
+    """
+    with open(filename, mode) as f:
+        f.write(doc)
+
+
+def main(df, filename, mode='w'):
+    """
+    Combines all functions in utils.py into one function
+    """
+    doc = df_to_xml(df)
+    write_xml(doc, filename, mode)
